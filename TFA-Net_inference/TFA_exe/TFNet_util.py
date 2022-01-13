9e0146c61e6f0746e5d694982e711a388961e006
panpp-git <77284474+panpp-git@users.noreply.github.com>
2022-01-13 10:32:03 +0800

    Add TFA-Net inference codes

---
+++ TFA-Net_inference/TFA_exe/TFNet_util.py
@@ -0,0 +1,96 @@
+import os
+import torch
+import errno
+import complexModule
+
+def model_parameters(model):
+    num_params = 0
+    for param in model.parameters():
+        num_params += param.numel()
+    return num_params
+
+
+def symlink_force(target, link_name):
+    try:
+        os.symlink(target, link_name)
+    except OSError as e:
+        if e.errno == errno.EEXIST:
+            os.remove(link_name)
+            os.symlink(target, link_name)
+        else:
+            raise e
+
+
+def save(model, optimizer, scheduler, args, epoch, module_type):
+    checkpoint = {
+            'epoch': epoch,
+            'model': model.state_dict(),
+            'optimizer': optimizer.state_dict(),
+            'scheduler': scheduler.state_dict(),
+            'args': args,
+    }
+    if scheduler is not None:
+        checkpoint["scheduler"] = scheduler.state_dict()
+    if not os.path.exists(os.path.join(args.output_dir, module_type)):
+        os.makedirs(os.path.join(args.output_dir, module_type))
+    cp = os.path.join(args.output_dir, module_type, 'last.pth')
+    fn = os.path.join(args.output_dir, module_type, 'epoch_'+str(epoch)+'.pth')
+    torch.save(checkpoint, fn)
+    symlink_force(fn, cp)
+
+
+def load(fn, module_type, device = torch.device('cuda')):
+    checkpoint = torch.load(fn, map_location=device)
+    args = checkpoint['args']
+    if device == torch.device('cpu'):
+        args.use_cuda = False
+    if module_type == 'skip':
+        model = complexModule.set_skip_module(args)
+    elif module_type == 'layer1':
+        model = complexModule.set_layer1_module(args)
+    else:
+        raise NotImplementedError('Module type not recognized')
+    model.load_state_dict(checkpoint['model'])
+    optimizer, scheduler = set_optim(args, model, module_type)
+    if checkpoint["scheduler"] is not None:
+        scheduler.load_state_dict(checkpoint["scheduler"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
+    return model, optimizer, scheduler, args, checkpoint['epoch']
+
+
+def set_optim(args, module, module_type):
+    if module_type == 'fr':
+        # params = list(module.parameters()) + list(adaptive.parameters())
+        optimizer = torch.optim.Adam(module.parameters(), lr=args.lr_fr)
+    elif module_type == 'fc':
+        optimizer = torch.optim.Adam(module.parameters(), lr=args.lr_fc)
+    elif module_type == 'skip':
+        optimizer = torch.optim.RMSprop(module.parameters(), lr=args.lr_fr, alpha=0.9)
+        # optimizer = torch.optim.Adam(module.parameters(), lr=args.lr_fr)
+    elif module_type == 'rdn':
+        optimizer = torch.optim.RMSprop(module.parameters(), lr=args.lr_fr, alpha=0.9)
+        # optimizer = torch.optim.Adam(module.parameters(), lr=args.lr_fr)
+    elif module_type == 'freq':
+        optimizer = torch.optim.RMSprop(module.parameters(), lr=args.lr_fr, alpha=0.9)
+        # optimizer = torch.optim.Adam(module.parameters(), lr=args.lr_fr)
+    elif module_type == 'deepfreqRes':
+        optimizer = torch.optim.RMSprop(module.parameters(), lr=args.lr_fr, alpha=0.9)
+    elif module_type == 'layer1':
+        optimizer = torch.optim.RMSprop(module.parameters(), lr=args.lr_fr, alpha=0.9)
+
+    else:
+        raise(ValueError('Expected module_type to be fr or fc but got {}'.format(module_type)))
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=7, factor=0.5, verbose=True)
+    return optimizer, scheduler
+
+
+def print_args(logger, args):
+    message = ''
+    for k, v in sorted(vars(args).items()):
+        message += '\n{:>30}: {:<30}'.format(str(k), str(v))
+    logger.info(message)
+
+    args_path = os.path.join(args.output_dir, 'run.args')
+    with open(args_path, 'wt') as args_file:
+        args_file.write(message)
+        args_file.write('\n')
---
+++ TFA-Net_inference/TFA_exe/complexModule.py
@@ -0,0 +1,225 @@
+import torch.nn as nn
+import torch
+from complexLayers import ComplexConv1d
+
+
+
+def set_layer1_module(args):
+    """
+    Create a frequency-representation module
+    """
+    net = None
+    if args.fr_module_type == 'fr':
+        assert args.fr_size == args.fr_inner_dim * args.fr_upsampling, \
+            'The desired size of the frequency representation (fr_size) must be equal to inner_dim*upsampling'
+        net = FrequencyRepresentationModule_TFA_Net(signal_dim=args.signal_dim, n_filters=args.fr_n_filters,
+                                            inner_dim=args.fr_inner_dim, n_layers=args.fr_n_layers,
+                                            upsampling=args.fr_upsampling, kernel_size=args.fr_kernel_size,
+                                            kernel_out=args.fr_kernel_out)
+        # net = FrequencyRepresentationModule_RED_Net(signal_dim=args.signal_dim, n_filters=args.fr_n_filters,
+        #                                     inner_dim=args.fr_inner_dim, n_layers=args.fr_n_layers,
+        #                                     upsampling=args.fr_upsampling, kernel_size=args.fr_kernel_size,
+        #                                     kernel_out=args.fr_kernel_out)
+
+    else:
+        raise NotImplementedError('Frequency representation module type not implemented')
+    if args.use_cuda:
+        net.cuda()
+    return net
+
+import math
+class REDNet30_stft(nn.Module):
+    def __init__(self, num_layers=15, num_features=8):
+        super(REDNet30_stft, self).__init__()
+        self.num_layers = num_layers
+
+        conv_layers = []
+        deconv_layers = []
+
+        conv_layers.append(nn.Sequential(nn.Conv2d(num_features, num_features*2, kernel_size=3, stride=2, padding=1),
+                                         nn.ReLU(inplace=True)))
+        for i in range(num_layers - 1):
+            conv_layers.append(nn.Sequential(nn.Conv2d(num_features*2, num_features*2, kernel_size=3, padding=1),
+                                             nn.ReLU(inplace=True)))
+
+        for i in range(num_layers - 1):
+            deconv_layers.append(nn.Sequential(nn.ConvTranspose2d(num_features*2, num_features*2, kernel_size=3, padding=1),
+                                               nn.ReLU(inplace=True)))
+        deconv_layers.append(nn.ConvTranspose2d(num_features*2, num_features, kernel_size=3, stride=2, padding=1, output_padding=1))
+
+        self.conv_layers = nn.Sequential(*conv_layers)
+        self.deconv_layers = nn.Sequential(*deconv_layers)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+
+        conv_feats = []
+        for i in range(self.num_layers):
+            x = self.conv_layers[i](x)
+            if (i + 1) % 2 == 0 and len(conv_feats) < math.ceil(self.num_layers / 2) - 1:
+                conv_feats.append(x)
+
+        conv_feats_idx = 0
+        for i in range(self.num_layers):
+            x = self.deconv_layers[i](x)
+            if (i + 1 + self.num_layers) % 2 == 0 and conv_feats_idx < len(conv_feats):
+                conv_feat = conv_feats[-(conv_feats_idx + 1)]
+                conv_feats_idx += 1
+                x = x + conv_feat
+                x = self.relu(x)
+
+        x += residual
+        x = self.relu(x)
+
+        return x
+class REDNet30(nn.Module):
+    def __init__(self, num_layers=15, num_features=8):
+        super(REDNet30, self).__init__()
+        self.num_layers = num_layers
+
+        conv_layers = []
+        deconv_layers = []
+
+        conv_layers.append(nn.Sequential(nn.Conv2d(num_features, num_features*2, kernel_size=3, stride=2, padding=1),
+                                         nn.ReLU(inplace=True)))
+        for i in range(num_layers - 1):
+            conv_layers.append(nn.Sequential(nn.Conv2d(num_features*2, num_features*2, kernel_size=3, padding=1),
+                                             nn.ReLU(inplace=True)))
+
+        for i in range(num_layers - 1):
+            deconv_layers.append(nn.Sequential(nn.ConvTranspose2d(num_features*2, num_features*2, kernel_size=3, padding=1),
+                                               nn.ReLU(inplace=True)))
+        deconv_layers.append(nn.ConvTranspose2d(num_features*2, num_features, kernel_size=3, stride=2, padding=1, output_padding=1))
+
+        self.conv_layers = nn.Sequential(*conv_layers)
+        self.deconv_layers = nn.Sequential(*deconv_layers)
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x):
+        residual = x
+
+        conv_feats = []
+        for i in range(self.num_layers):
+            x = self.conv_layers[i](x)
+            if (i + 1) % 2 == 0 and len(conv_feats) < math.ceil(self.num_layers / 2) - 1:
+                conv_feats.append(x)
+
+        conv_feats_idx = 0
+        for i in range(self.num_layers):
+            x = self.deconv_layers[i](x)
+            if (i + 1 + self.num_layers) % 2 == 0 and conv_feats_idx < len(conv_feats):
+                conv_feat = conv_feats[-(conv_feats_idx + 1)]
+                conv_feats_idx += 1
+                x = x + conv_feat
+                x = self.relu(x)
+
+        x += residual
+        x = self.relu(x)
+
+        return x
+
+class FrequencyRepresentationModule_TFA_Net(nn.Module):
+    def __init__(self, signal_dim=50, n_filters=8, n_layers=3, inner_dim=125,
+                 kernel_size=3, upsampling=2, kernel_out=3):
+        super().__init__()
+
+        self.n_filters = n_filters
+        self.inner = inner_dim
+        self.n_layers = n_layers
+        # self.dnoise1=ComplexConv1d2(1,n_filters,kernel_size=(1,5),padding=(0,5//2))
+        # self.dnoise2 = ComplexConv1d(1, n_filters, kernel_size=(1, 7), padding=(0, 7// 2))
+        # self.concat_layer=ComplexConv1d(2*n_filters,n_filters,kernel_size=(1,1))
+        self.in_layer1 = ComplexConv1d(1, inner_dim * n_filters, kernel_size=(1, 31), padding=(0, 31 // 2),
+                                       bias=False)
+        # self.win = ComplexConv2d(n_filters, n_filters, kernel_size=(127, 1), padding=(127 // 2, 0),
+        #                          bias=False,padding_mode='circular')
+        self.rednet=REDNet30(self.n_layers,num_features=n_filters)
+        self.out_layer = nn.ConvTranspose2d(n_filters, 1, (3, 1), stride=(upsampling, 1),
+                                            padding=(1, 0), output_padding=(1, 0), bias=False)
+        # self.out_layer2=nn.ReLU()
+        # self.out_layer3=nn.Conv2d(n_filters//2, 1, (3, 3),padding=(1, 1))
+
+    def forward(self, x):
+        bsz = x.size(0)
+        inp_real = x[:, 0, :].view(bsz, 1, 1, -1)
+        inp_imag = x[:, 1, :].view(bsz, 1, 1, -1)
+        inp = torch.cat((inp_real, inp_imag), 1)
+        # x1=self.dnoise1(inp)
+        # x1real,x1imag=torch.chunk(x1,2,1)
+        # x2 = self.dnoise2(inp)
+        # x2real, x2imag = torch.chunk(x2, 2, 1)
+        # x=torch.cat((x1real,x2real,x1imag,x2imag),1)
+        # x=self.concat_layer(x)
+        x1 = self.in_layer1(inp)
+        xreal,ximag=torch.chunk(x1,2,1)
+        xreal = xreal.view(bsz, self.n_filters, self.inner, -1)
+        ximag = ximag.view(bsz, self.n_filters, self.inner, -1)
+        # x = torch.cat((xreal, ximag), 1)
+        # x=self.win(x)
+        # xreal,ximag=torch.chunk(x,2,1)
+        x=torch.sqrt(torch.pow(xreal, 2) + torch.pow(ximag, 2))
+        x=self.rednet(x)
+
+        x = self.out_layer(x).squeeze(-3).transpose(1, 2)
+        # x=self.out_layer2(x)
+        # x=self.out_layer3(x)
+        return x
+
+class FrequencyRepresentationModule_RED_Net(nn.Module):
+    def __init__(self, signal_dim=50, n_filters=8, n_layers=3, inner_dim=125,
+                 kernel_size=3, upsampling=2, kernel_out=3):
+        super().__init__()
+
+        self.n_filters = n_filters
+        self.inner = inner_dim
+        self.n_layers = n_layers
+        self.in_layer=nn.Conv2d(4, n_filters, kernel_size=1)
+        self.rednet=REDNet30_stft(self.n_layers,num_features=n_filters)
+        self.out_layer = nn.ConvTranspose2d(n_filters, 1, (3, 1), stride=(upsampling, 1),
+                                            padding=(1, 0), output_padding=(1, 0), bias=False)
+
+        # self.out_layer2=nn.ReLU()
+        # self.out_layer3=nn.Conv2d(n_filters//2, 1, (3, 3),padding=(1, 1))
+
+    def forward(self, x):
+        bsz = x.size(0)
+        xreal = x[:, 0, :].view(bsz, 1, 1, -1)
+        ximag = x[:, 1, :].view(bsz, 1, 1, -1)
+
+        xreal2 = x[:, 2, :].view(bsz, 1, 1, -1)
+        ximag2 = x[:, 3, :].view(bsz, 1, 1, -1)
+
+        xreal3 = x[:, 4, :].view(bsz, 1, 1, -1)
+        ximag3 = x[:, 5, :].view(bsz, 1, 1, -1)
+
+        xreal4 = x[:, 6, :].view(bsz, 1, 1, -1)
+        ximag4 = x[:, 7, :].view(bsz, 1, 1, -1)
+
+        xreal = xreal.view(bsz, 1, self.inner, -1)
+        ximag = ximag.view(bsz, 1, self.inner, -1)
+
+        xreal2 = xreal2.view(bsz, 1, self.inner, -1)
+        ximag2 = ximag2.view(bsz, 1, self.inner, -1)
+
+        xreal3 = xreal3.view(bsz, 1, self.inner, -1)
+        ximag3 = ximag3.view(bsz, 1, self.inner, -1)
+
+        xreal4 = xreal4.view(bsz, 1, self.inner, -1)
+        ximag4 = ximag4.view(bsz, 1, self.inner, -1)
+
+        x=torch.sqrt(torch.pow(xreal, 2) + torch.pow(ximag, 2))
+        x2 = torch.sqrt(torch.pow(xreal2, 2) + torch.pow(ximag2, 2))
+        x3 = torch.sqrt(torch.pow(xreal3, 2) + torch.pow(ximag3, 2))
+        x4 = torch.sqrt(torch.pow(xreal4, 2) + torch.pow(ximag4, 2))
+        x=torch.cat((x,x2,x3,x4),1)
+        x=self.in_layer(x)
+        x=self.rednet(x)
+        x = self.out_layer(x).squeeze(-3).transpose(1, 2)
+        # x=self.out_layer2(x)
+        # x=self.out_layer3(x)
+        return x
+
+
+
+
